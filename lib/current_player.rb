9c55e086c88890ced1a4088b3c254e4282e8fb87
Alexandra Fren <alexandra.fren@gmail.com>
2018-05-30 03:02:12 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+def turn_count(board)
+  turn_count.each do |counts|
+    if counts == "X" || counts == "O"
+      counter += 1
+    end
+  end
+
+  def current_player(display_board)
+    turn_count(board)
+    if turn_count % 2
+      return "X"
+    else
+      return "O"
+    end
+  end
+  
