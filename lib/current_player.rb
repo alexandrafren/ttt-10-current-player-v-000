2666748563ca0ee536871fe50c48011872894ae8
Alexandra Fren <alexandra.fren@gmail.com>
2018-05-30 03:04:24 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,18 +1,9 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def turn_count(board)
-  turn_count.each do |counts|
+  turn_count.each do |spaces|
     if counts == "X" || counts == "O"
       counter += 1
     end
   end
 end
-
-  def current_player(display_board)
-    turn_count(board)
-    if turn_count % 2
-      return "X"
-    else
-      return "O"
-    end
-  end
